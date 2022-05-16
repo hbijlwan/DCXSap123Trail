f1f4d02ad221569ef70ad93afe5f54151ac63a24
Bijlwan <himanshu.bijlwan@capgemini.com>
2022-05-16 19:33:14 +0530

    Initial Commit

---
+++ custom/concerttours/gensrc/concerttours/constants/GeneratedConcerttoursConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at May 3, 2022, 5:18:53 PM                     ---
+ * ----------------------------------------------------------------
+ */
+package concerttours.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated(since = "ages", forRemoval = false)
+@SuppressWarnings({"unused","cast"})
+public class GeneratedConcerttoursConstants
+{
+	public static final String EXTENSIONNAME = "concerttours";
+	
+	protected GeneratedConcerttoursConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
